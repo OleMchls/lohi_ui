9eab15d8478353934d31e3a3fe18518fc6f3909e
Ole Michaelis <Ole.Michaelis@googlemail.com>
2018-08-30 16:35:01 +0200

    add controls scaffor for hw interaction

---
+++ lib/lohi_ui/controls.ex
@@ -0,0 +1,19 @@
+defmodule LohiUi.Controls do
+
+  def tag(uuid) do
+    IO.inspect(uuid, label: "RFID Tag Scanned")
+  end
+
+  def play do
+
+  end
+
+  def volume_up do
+
+  end
+
+  def volume_down do
+
+  end
+
+end
