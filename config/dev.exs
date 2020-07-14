3da8adf464e2aaacbe55602e689a052890f5ccc8
Ole Michaelis <Ole.Michaelis@googlemail.com>
2020-07-14 12:24:38 +0200

    re-enable libcluster

--- config/dev.exs
+++ config/dev.exs
@@ -72,14 +72,10 @@ config :libcluster,
     lohi: [
       strategy: Elixir.Cluster.Strategy.Gossip,
       config: [
-        port: 45892,
-        if_addr: "0.0.0.0",
-        multicast_addr: "192.168.178.255",
-        multicast_ttl: 1,
-        secret: "6PvqGXelxXCyIlhh7IJf"
-      ],
-      connect: {:net_kernel, :connect_node, []},
-      disconnect: {:erlang, :disconnect_node, []},
-      list_nodes: {:erlang, :nodes, [:connected]}
+        # port: 45892,
+        # if_addr: "0.0.0.0",
+        multicast_addr: "192.168.178.255"
+        # multicast_ttl: 1,
+      ]
     ]
   ]
